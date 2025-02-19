637726a0dd548eb0badb95975ff13600fc899bc5
Samuel B. <2351016@csfoy.ca>
2025-02-19 08:12:40 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+import TimeLine from './components/TimeLine.vue'
+</script>
+
+<template>
+  <div class="container">
+    <TimeLine />
+    <img src="/src/assets/img/funny-cat.jpg" alt="chat très drole">
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TimeLineItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {type TimelinePost} from "../posts"
+
+
+defineProps<{
+post: TimelinePost
+}>()
+
+</script>
+<template>
+     <li>
+        <span>{{ post.title }}</span>&nbsp;|&nbsp;
+        <span>{{ post.created.toFormat("d MMMM") }}</span>
+        </li>
+</template>
+<style>
+</style>
